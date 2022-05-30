73a7bd0203e1cf45e76b3d6c38aedfd8362d1f6a
i-tanaka730 <pttp730@gmail.com>
2022-05-30 22:42:26 +0900

    feat: モデルの実装。

--- src/lib/model/training_item.dart
+++ src/lib/model/training_item.dart
@@ -1,1 +1,9 @@
-class TrainingItem {}
+class TrainingItem {
+  String name;
+  int count;
+
+  TrainingItem(this.name, this.count);
+
+  void changeName(String name) => this.name = name;
+  void changeCount(int count) => this.count = count;
+}
