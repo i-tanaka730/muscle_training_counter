fd32a077cc3cf47a13d8ec2bd5bb34b51b72513a
i-tanaka730 <pttp730@gmail.com>
2022-06-28 22:49:46 +0900

    feat: 一覧が表示されるように対応中。

--- src/lib/main.dart
+++ src/lib/main.dart
@@ -1,6 +1,6 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:muscle_training_counter/ui/list/training_list_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized(); //←Firebaseを初期化する際に必要らしい
@@ -14,78 +14,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: '筋トレカウンター',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: '筋トレカウンター'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  @override
-  void initState() {
-    super.initState();
-    load();
-  }
-
-  void load() async {
-    await FirebaseFirestore.instance.collection('menu').doc('腕立て伏せ').get().then((DocumentSnapshot snapshot) {
-      var count = snapshot.get('count');
-      debugPrint(count.toString());
-      setState(() {
-        _counter = count as int;
-      });
-    });
-  }
-
-  void _incrementCounter() async {
-    setState(() {
-      _counter++;
-    });
-    // ドキュメント作成
-    await FirebaseFirestore.instance
-        .collection('menu') // コレクションID
-        .doc('腕立て伏せ') // ドキュメントID
-        .set({'count': _counter}); // データ
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'カウンター',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+      home: const TraningListPage(),
     );
   }
 }
--- src/lib/ui/list/training_list_page.dart
+++ src/lib/ui/list/training_list_page.dart
@@ -1,1 +1,88 @@
-class TrainingListPage {}
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
+
+import '../../model/training_item.dart';
+
+class TraningListPage extends StatefulWidget {
+  const TraningListPage({Key? key}) : super(key: key);
+
+  @override
+  State<TraningListPage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<TraningListPage> {
+  int _counter = 0;
+  List<TrainingItem> _traningItemlist = [
+    TrainingItem("腕立て伏せ", 1500),
+    TrainingItem("腹筋", 800),
+    TrainingItem("スクワット", 3000)
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+    load();
+  }
+
+  void load() async {
+    await FirebaseFirestore.instance.collection('menu').doc('腕立て伏せ').get().then((DocumentSnapshot snapshot) {
+      var count = snapshot.get('count');
+      debugPrint(count.toString());
+      setState(() {
+        _counter = count as int;
+      });
+    });
+  }
+
+  void _incrementCounter() async {
+    setState(() {
+      _counter++;
+    });
+    // ドキュメント作成
+    await FirebaseFirestore.instance
+        .collection('menu') // コレクションID
+        .doc('腕立て伏せ') // ドキュメントID
+        .set({'count': _counter}); // データ
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('筋トレ一覧'),
+      ),
+      body: ListView.builder(
+          itemCount: _traningItemlist.length,
+          itemBuilder: (context, index) {
+            var item = _traningItemlist[index];
+            return Slidable(
+              child: Row(
+                //decoration: const BoxDecoration(border: Border(bottom: BorderSide(color: Colors.grey))),
+                children: <Widget>[
+                  Text(item.getName()),
+                  Text(_counter.toString()),
+                  ButtonBar(
+                    children: <Widget>[
+                      ElevatedButton(
+                        child: const Text('+'),
+                        onPressed: () {},
+                      ),
+                      ElevatedButton(
+                        child: const Text('-'),
+                        onPressed: () {},
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            );
+          }),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
